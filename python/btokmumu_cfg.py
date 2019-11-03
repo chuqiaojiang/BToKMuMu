351fb4d12e33eef8d2bec6d1383c13a6c3a399a5
chuqiaojiang <53284941+chuqiaojiang@users.noreply.github.com>
2019-11-03 21:50:16 +0800

    Update btokmumu_cfg.py

--- python/btokmumu_cfg.py
+++ python/btokmumu_cfg.py
@@ -17,6 +17,26 @@ triggerProcessName = 'HLT'
 
 
 
+
+#注释20191103
+'''
+process.cleanMuonTriggerMatchHLT0 = cms.EDProducer(
+        # match by DeltaR only (best match by DeltaR)
+        'PATTriggerMatcherDRLessByR',
+            src                   = cms.InputTag('cleanPatMuons'),
+            # default producer label as defined in
+            # PhysicsTools/PatAlgos/python/triggerLayer1/triggerProducer_cfi.py
+            matched               = cms.InputTag('patTrigger'),
+            matchedCuts           = cms.string('path("HLT_DoubleMu3p5_LowMass_Displaced*",0,0)'),
+            maxDeltaR             = cms.double(0.1),
+            # only one match per trigger object
+            resolveAmbiguities    = cms.bool(True),
+            # take best match found per reco object (by DeltaR here, see above)
+            resolveByMatchQuality = cms.bool(False))
+'''
+
+
+
 from PhysicsTools.PatAlgos.tools.trigTools import *
 switchOnTriggerMatchEmbedding(process, triggerMatchers = ['cleanMuonTriggerMatchHLT0'],
                                hltProcess = triggerProcessName, outputModule = '')
